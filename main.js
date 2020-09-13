2f890e71c96a55923eebdfd556cbd35b605d2711
Dennis Hinzpeter <o0shojo0o@dietru.de>
2020-09-13 17:16:57 +0200

    Update

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 'use strict';
 
 const utils = require('@iobroker/adapter-core');
+const WebSocket = require('ws');
+let wsClient;
 const adapterName = require('./package.json').name.split('.').pop();
 
-class Seq extends utils.Adapter {
+class PixelIt extends utils.Adapter {
 
     constructor(options) {
         super({
@@ -15,16 +17,30 @@ class Seq extends utils.Adapter {
         this.on('unload', this.onUnload.bind(this));
     }
 
-    onReady() {
-
+    async onReady() {
         // Get Config
-        const _serverUrl = this.config.address;
+        const _pixelItAddress = this.config.address;
 
         // Check Server address
-        if (!_serverUrl || _serverUrl === '' || !(_serverUrl.startsWith('http://') && _serverUrl.startsWith('http://'))) {
-            this.log.warn('Server address is not a valid, please check your settings!')
+        if (!_pixelItAddress || _pixelItAddress === '') {
+            this.log.warn('PixelIt address is not a valid, please check your settings!')
             return;
         }
+
+        CreateDataPoints(this);
+
+        wsClient = new WebSocket('ws://' + _pixelItAddress + ':81/dash');
+        let _adapter = this;
+        wsClient.on('message', function incoming(data) {
+            let msgObj = JSON.parse(data);
+            if (msgObj.ipAddress !== 'undefined') {
+                _adapter.setStateAsync('config.ip', {
+                    val: msgObj.ipAddress,
+                    ack: true
+                });
+            }
+            console.log(data);
+        });
     }
 
     onUnload(callback) {
@@ -36,8 +52,132 @@ class Seq extends utils.Adapter {
     }
 }
 
+async function CreateDataPoints(adapter) {
+
+    await adapter.setObjectNotExistsAsync('info.ip', {
+        type: 'state',
+        common: {
+            name: 'IP-Address',
+            type: 'string',
+            role: 'info.ip',
+            read: true,
+            write: false,
+        },
+        native: {},
+    });
+
+    await adapter.setObjectNotExistsAsync('info.version', {
+        type: 'state',
+        common: {
+            name: 'Version',
+            type: 'string',
+            role: '',
+            read: true,
+            write: false,
+        },
+        native: {},
+    });
+
+    await adapter.setObjectNotExistsAsync('info.sketchsize', {
+        type: 'state',
+        common: {
+            name: 'Sketch Size',
+            type: 'string',
+            role: '',
+            read: true,
+            write: false,
+        },
+        native: {},
+    });
+
+    await adapter.setObjectNotExistsAsync('info.freesketchspace', {
+        type: 'state',
+        common: {
+            name: 'Free Sketch Space',
+            type: 'string',
+            role: '',
+            read: true,
+            write: false,
+        },
+        native: {},
+    });
+
+    await adapter.setObjectNotExistsAsync('info.wifirssi', {
+        type: 'state',
+        common: {
+            name: 'Wifi RSSI',
+            type: 'string',
+            role: '',
+            read: true,
+            write: false,
+        },
+        native: {},
+    });
+
+    await adapter.setObjectNotExistsAsync('info.wifiquality', {
+        type: 'state',
+        common: {
+            name: 'Wifi Quality',
+            type: 'string',
+            role: '',
+            read: true,
+            write: false,
+        },
+        native: {},
+    });
+
+    await adapter.setObjectNotExistsAsync('info.wifissid', {
+        type: 'state',
+        common: {
+            name: 'Wifi SSID',
+            type: 'string',
+            role: '',
+            read: true,
+            write: false,
+        },
+        native: {},
+    });
+
+    await adapter.setObjectNotExistsAsync('info.freeheap', {
+        type: 'state',
+        common: {
+            name: 'Free Heap',
+            type: 'string',
+            role: '',
+            read: true,
+            write: false,
+        },
+        native: {},
+    });
+
+    await adapter.setObjectNotExistsAsync('info.chipid', {
+        type: 'state',
+        common: {
+            name: 'Chip ID',
+            type: 'string',
+            role: '',
+            read: true,
+            write: false,
+        },
+        native: {},
+    });
+
+    await adapter.setObjectNotExistsAsync('info.cpufreq', {
+        type: 'state',
+        common: {
+            name: 'CPU-Freq (MHz)',
+            type: 'string',
+            role: '',
+            read: true,
+            write: false,
+        },
+        native: {},
+    });
+}
+
+
 if (module.parent) {
-    module.exports = (options) => new Seq(options);
+    module.exports = (options) => new PixelIt(options);
 } else {
-    new Seq();
+    new PixelIt();
 }
