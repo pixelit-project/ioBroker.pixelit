4cf4743fe44444a44a96629584ab113b87246434
Dennis Rathjen <dennis.rathjen@outlook.de>
2024-09-02 20:51:12 +0200

    Delete words.js

--- admin/words.js
+++
@@ -1,7 +0,0 @@
-/*global systemDictionary:true */
-'use strict';
-
-systemDictionary = {
-    "IP address": {                                  "en": "IP address",                                      "de": "IP Adresse",                                      "ru": "айпи адрес",                                      "pt": "endereço de IP",                                  "nl": "IP adres",                                        "fr": "adresse IP",                                      "it": "indirizzo IP",                                    "es": "dirección IP",                                    "pl": "adres IP",                                        "zh-cn": "IP地址"},
-    "Poll Interval (sec.)": {                        "en": "Poll Interval (sec.)",                            "de": "Abfrageintervall (Sek.)",                         "ru": "Интервал опроса (сек.)",                          "pt": "Intervalo de sondagem (seg.)",                    "nl": "Poll-interval (sec.)",                            "fr": "Intervalle d'interrogation (sec.)",               "it": "Intervallo sondaggio (sec.)",                     "es": "Intervalo de encuesta (seg.)",                    "pl": "Interwał sondowania (sek.)",                      "zh-cn": "轮询间隔（秒）"},
-};
